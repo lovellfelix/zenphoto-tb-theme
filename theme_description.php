0502195c3ae622c5fd0b3d6f0ffff84dcee100ec
Lovell Felix <lovell.felix@gmail.com>
2014-05-25 10:34:52 -0500

    updated theme description

--- theme_description.php
+++ theme_description.php
@@ -1,8 +1,9 @@
 <?php
 // Zenphoto theme definition file
-$theme_description['name'] = 'ZenPhoto Twitter Bootstrap - CSOWEB.ORG';
+$theme_description['name'] = 'ZenPhoto Twitter Bootstrap';
 $theme_description['author'] = gettext('Lovell Felix');
-$theme_description['version'] = true;
-$theme_description['date'] = true;
-$theme_description['desc'] = gettext("ZenPhoto Theme built with Twitter Bootstrap");
+$theme_description['version'] = '0.1';
+$theme_description['date'] = '5/28/14';
+$theme_description['desc'] = gettext("ZenPhoto Theme create with Twitter Bootstrap");
+
 ?>
