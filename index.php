4e086bdef50acd8d863c0e1c2715f1d5bf42c5a0
Lovell Felix <lovell.felix@gmail.com>
2014-05-25 23:29:58 -0500

    Added album count and mute color

--- index.php
+++ index.php
@@ -13,9 +13,43 @@
                 </div>
                 <div class="panel-footer">
                      <h4><a href="<?php echo html_encode(getAlbumLinkURL()); ?>">
+                           <span class="text-muted glyphicon glyphicon-book"></span>
+
+                           <a class="text-muted" href="<?php echo html_encode(getAlbumLinkURL()); ?>">
+
 	                     <?php printAlbumTitle(); ?></h4></a>
-	                     <p><?php printAlbumDesc(); ?></p>
+	                     
+	        <?php
+			$anumber = getNumAlbums();
+			$inumber = getNumImages();
+			if ($anumber > 0 || $inumber > 0) {
+				echo '<a class="text-muted" href='. html_encode(getAlbumLinkURL()). '> ';
+				if ($anumber == 0) {
+					if ($inumber != 0) {
+						printf(ngettext('%u image','%u images', $inumber), $inumber);
+					}
+				} else if ($anumber == 1) {
+					if ($inumber > 0) {
+						printf(ngettext('1 Album,&nbsp;%u image','1 Album,&nbsp;%u images', $inumber), $inumber);
+					} else {
+						printf(gettext('1 Album'));
+					}
+				} else {
+					if ($inumber == 1) {
+						printf(ngettext('%u Album,&nbsp;1 image','%u Albums,&nbsp;1 image', $anumber), $anumber);
+					} else if ($inumber > 0) {
+						printf(ngettext('%1$u Album,&nbsp;%2$s','%1$u Albums,&nbsp;%2$s', $anumber), $anumber, sprintf(ngettext('%u image','%u images',$inumber),$inumber));
+					} else {
+						printf(ngettext('%u Album','%u Albums', $anumber), $anumber);
+					}
+				}
+				echo '</a><br />';
+			}
+			echo shortenContent(strip_tags(getAlbumDesc()), 50, '...');
+		?>
                     <span class="pull-right">
+                    
+                    
 
                     </span>
                 </div>
